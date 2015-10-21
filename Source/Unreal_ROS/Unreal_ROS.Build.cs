9a1526d5f36c27b73f34d7bf675ee451aedfe8d1
xuhao <xuhao3e8@gmail.com>
2015-10-21 14:30:15 +0800

    File Structre

---
+++ Source/Unreal_ROS/Unreal_ROS.Build.cs
@@ -0,0 +1,29 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+namespace UnrealBuildTool.Rules
+{
+	public class Unreal_ROS : ModuleRules
+	{
+		public Unreal_ROS(TargetInfo Target)
+		{
+			//PrivateIncludePaths.AddRange(
+			//	);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					"Engine",
+					"InputCore"
+				}
+				);
+
+			if (UEBuildConfiguration.bBuildEditor == true)
+			{
+				PrivateDependencyModuleNames.Add("UnrealEd");
+			}
+
+		}
+	}
+}
